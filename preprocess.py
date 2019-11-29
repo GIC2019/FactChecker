07987d09709043fd2b621b7c8f78a66bfe431f3f
Paul Fährmann <paulf@iesy.net>
2019-11-29 16:43:20 +0100

    updated preprocess to include cleaning of special letters

--- preprocess.py
+++ preprocess.py
@@ -2,21 +2,34 @@ import inspect
 import os
 import re
 
+special = "ŞŠŽšžŸÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðñòóôõöùúûüýÿ"
+normal = "SSZszYAAAAAACEEEEIIIIDNOOOOOUUUUYaaaaaaceeeeiiiidnooooouuuuyy"
+
+def clean_special_letters(text, special, normal):
+    clean = text
+    for i in range(len(special)):
+        clean = clean.replace(special[i], normal[i])
+    return clean
+
 def elabFile(filein,fileout):
 	in_file = open(filein,"r", encoding="utf-8")
 	out_file = open(fileout, "a", encoding="utf-8")
 	line = in_file.readline()
 	cnt = 1
 	while line:
-		line = re.sub(r"[^a-zA-Z0-9.!? ]", " ", line)
-		line = re.sub(r"[.!?]", " </s> <s> ", line)
-		line = "<s> " + line.strip() + " </s>"
-		line = line.replace("<s> </s>", " ")
+		if not (line == "==========\n"):
+			line = clean_special_letters(line, special, normal)
+			line = re.sub(r"[^a-zA-Z0-9.!? ]", " ", line)
+			line = re.sub(r"[.!?]", " </s> <s> ", line)
+			line = "<s> " + line.strip() + " </s>"
+			line = line.replace("<s> </s>", " ")
 		
-		line = line.lower()
+			line = line.lower()
 		
-		token = line.split(" +")
-		out_file.write(line)
+			token = line.split(" +")
+			out_file.write(line+'\n')
+		else:
+			out_file.write(line)
 		line = in_file.readline()
 		cnt += 1
 	in_file.close()
@@ -24,6 +37,8 @@ def elabFile(filein,fileout):
 
 	
 dirname = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
-filein = os.path.join(dirname, 'corpus00_small.txt')
-fileout = os.path.join(dirname, 'corpus00_small_tokenized.txt')
-elabFile(filein,fileout)
+for i in range(39):
+	filename = 'corpus0'+str(i)
+	filein = os.path.join(dirname,filename+'.txt')
+	fileout = os.path.join(dirname, filename+'_tokenized.txt')
+	elabFile(filein,fileout)
